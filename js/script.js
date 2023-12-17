e31ba65b2e3749ab887abbb75d2168aac5db7267
Kaio Dias <kaiofd20062@gmail.com>
2023-12-17 15:21:15 -0300

    Finalização da responsividade e do menu mobile

---
+++ js/script.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var menuBtn = document.getElementById('menu-btn');
+    var menuBtnFechar = document.getElementById('btn-fechar');
+    var mobileMenu = document.getElementById('mobile-menu');
+    var content = document.querySelector('.content');
+
+    menuBtn.addEventListener('click', function() {
+        if (mobileMenu.style.right === "-250px") {
+            mobileMenu.style.right = "0";
+            content.style.marginRight = "250px";
+        } else {
+            mobileMenu.style.right = "-250px";
+            content.style.marginRight = "0";
+        }
+    });
+
+    menuBtnFechar.addEventListener('click', function() {
+        if (mobileMenu.style.right === "-250px") {
+            mobileMenu.style.right = "0";
+            content.style.marginRight = "250px";
+        } else {
+            mobileMenu.style.right = "-250px";
+            content.style.marginRight = "0";
+        }
+    });
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: -15,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+var bannerBtn = document.querySelector('.btn');
+
+function updateSwiper() {
+    // Verifique o tamanho da tela ou qualquer outra lógica que você preferir
+    if (window.innerWidth < 768) {
+        swiper.params.slidesPerView = 1; 
+        bannerBtn.innerText = "Levar";
+    } else if (window.innerWidth < 992) {
+        swiper.params.slidesPerView = 2; 
+        bannerBtn.innerText = "Levar";
+    } else {
+        swiper.params.slidesPerView = 4; 
+    }
+
+    // Atualize o swiper
+    swiper.update();
+}
+
+updateSwiper();
+
+
+window.addEventListener("resize", function () {
+    updateSwiper();
+});
