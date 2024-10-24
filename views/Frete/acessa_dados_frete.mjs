fbee38dc6563c5c989a2a5ead028f5de0db12868
Kaio Dias <kaiofd20062@gmail.com>
2024-10-24 18:26:57 -0300

    Reorganização de funcionalidades de frete

---
+++ views/Frete/acessa_dados_frete.mjs
@@ -0,0 +1,32 @@
+async function consultarCEP(cep) {
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        if (!response.ok) throw new Error("CEP não encontrado.");
+        const dados = await response.json();
+
+        if (dados.erro) throw new Error("CEP inválido.");
+        return dados; // Retorna os dados do CEP
+    } catch (error) {
+        console.error("Erro ao consultar CEP:", error);
+        alert("CEP inválido ou não encontrado.");
+    }
+}
+
+function calcularFrete(cep) {
+    const taxaFixa = 10;
+    const distancia = parseInt(cep.substring(0, 2)) * 0.5; 
+    const valorFrete = taxaFixa + distancia;
+    return valorFrete.toFixed(2);
+}
+
+function exibirFrete(endereco, frete) {
+    const resultadoFrete = document.querySelector(".resultado-frete");
+    resultadoFrete.innerHTML = `
+        <p><strong>Endereço:</strong> ${endereco.localidade} - ${endereco.uf}</p>
+        <p><strong>Frete:</strong> R$ ${frete}</p>
+    `;
+
+    //console.log(endereco);
+}
+
+export {consultarCEP, calcularFrete, exibirFrete};
--- views/Produto/manipula_interface_produto.mjs
+++ views/Produto/manipula_interface_produto.mjs
@@ -1,36 +1,5 @@
 import { altera, buscaUm, exclui, getLista, novo } from "./acessa_dados_produto.mjs";
-
-
-async function consultarCEP(cep) {
-    try {
-        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        if (!response.ok) throw new Error("CEP não encontrado.");
-        const dados = await response.json();
-
-        if (dados.erro) throw new Error("CEP inválido.");
-        return dados; // Retorna os dados do CEP
-    } catch (error) {
-        console.error("Erro ao consultar CEP:", error);
-        alert("CEP inválido ou não encontrado.");
-    }
-}
-
-function calcularFrete(cep) {
-    const taxaFixa = 10;
-    const distancia = parseInt(cep.substring(0, 2)) * 0.5; 
-    const valorFrete = taxaFixa + distancia;
-    return valorFrete.toFixed(2);
-}
-
-function exibirFrete(endereco, frete) {
-    const resultadoFrete = document.querySelector(".resultado-frete");
-    resultadoFrete.innerHTML = `
-        <p><strong>Endereço:</strong> ${endereco.localidade} - ${endereco.uf}</p>
-        <p><strong>Frete:</strong> R$ ${frete}</p>
-    `;
-
-    //console.log(endereco);
-}
+import {consultarCEP, calcularFrete, exibirFrete} from "../Frete/acessa_dados_frete.mjs"
 
 async function inserirProdPrincipal() {
     const boxcontainers = document.querySelectorAll('.vitrinePrincipal'); 
